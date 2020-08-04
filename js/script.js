ff6aafd3e365c14593d0b5d111ac8d33a3238f9a
Moly <64364914+molyrichez@users.noreply.github.com>
2020-08-04 23:45:28 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1275},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product","displayName":"product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1275},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product","displayName":"product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-products","displayName":"products","link":{"linkType":"LinkTypePage","href":"#!page-products"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
