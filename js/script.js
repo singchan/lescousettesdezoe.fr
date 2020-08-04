81ff25765092fdd5d9f5fa1edea547497caa0c8c
Moly <64364914+molyrichez@users.noreply.github.com>
2020-08-04 16:21:26 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1275},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-produits","displayName":"produits","link":{"linkType":"LinkTypePage","href":"#!page-produits"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1275},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product","displayName":"product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
