adb63e2dab07cab9d1bb459e40a2a831a734ce95
Moly <64364914+molyrichez@users.noreply.github.com>
2020-09-10 15:56:13 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1277},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product","displayName":"product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-tag","displayName":"tag","link":{"linkType":"LinkTypePage","href":"#!page-tag"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-commander","displayName":"commander","link":{"linkType":"LinkTypePage","href":"#!page-commander"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-qui","displayName":"qui","link":{"linkType":"LinkTypePage","href":"#!page-qui"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-default","displayName":"default","link":{"linkType":"LinkTypePage","href":"#!page-default"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-404","displayName":"404","link":{"linkType":"LinkTypePage","href":"#!page-404"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1277},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product","displayName":"product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-tag","displayName":"tag","link":{"linkType":"LinkTypePage","href":"#!page-tag"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-commander","displayName":"commander","link":{"linkType":"LinkTypePage","href":"#!page-commander"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-qui","displayName":"qui","link":{"linkType":"LinkTypePage","href":"#!page-qui"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-default","displayName":"default","link":{"linkType":"LinkTypePage","href":"#!page-default"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-404","displayName":"404","link":{"linkType":"LinkTypePage","href":"#!page-404"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
 $(function() {
-    var bar = document.querySelector('.silex-id-1597925748983-21')
+    var bar = document.querySelector('.bar')
 
     // bar position at the bottom of the screen even when scaled down by silex
     function updateBar() {
         var barHeight = $(bar).height() * silex.scale
-        var winHeight = $(window).height()
+        var winHeight = window.innerHeight
         bar.style.top = ((winHeight - barHeight) / silex.scale) + "px"
     }
     updateBar()
